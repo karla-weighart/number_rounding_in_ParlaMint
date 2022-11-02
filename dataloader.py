500cebca959de91a3410e01a32b001447ffc7ce2
Karla Weighart <karla.weighart@gmail.com>
2022-11-02 16:33:17 +0100

    added methods to access data file paths

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,16 @@
-import pandas as pd
 from glob import glob
 
+from environment_constants import PATH, YEARS
+
+
+def make_tsv_files_dict():
+    tsv_files_dict = {}
+    for year in YEARS:
+        tsv_files_dict[year] = [file_name for file_name in glob(PATH + "\\" + year + "\\*.tsv")]
+
+
+def make_conllu_files_dict():
+    conllu_files_dict = {}
+    for year in YEARS:
+        conllu_files_dict[year] = [file_name for file_name in glob(PATH+"\\"+year+"\\*.conllu")]
+    return conllu_files_dict
