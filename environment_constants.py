cf665b41662a8eddd77c29d1c3854562ee0f6509
Karla Weighart <karla.weighart@gmail.com>
2022-11-10 16:36:39 +0100

    renamed folder

--- environment_constants.py
+++ environment_constants.py
@@ -1,2 +1,2 @@
-PATH = "C:\\Users\\karla\\Desktop\\Zula - Data\\ParlaMint-GB.ana\\ParlaMint-GB.conllu"
+PATH = "C:\\Users\\karla\\Desktop\\Zula_Data\\ParlaMint-GB.ana\\ParlaMint-GB.conllu"
 YEARS = ["2015", "2016", "2017", "2018", "2019", "2020", "2021"]
