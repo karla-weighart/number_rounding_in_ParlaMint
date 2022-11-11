3ed047a66254b00075a0e82db93cf896d1f2d7b6
Karla Weighart <karla.weighart@gmail.com>
2022-11-11 20:01:46 +0100

    renamed check_if_column_empty to files_where_column_empty to make results more intuitively interpretable

--- understanding_the_corpus.py
+++ understanding_the_corpus.py
@@ -6,15 +6,15 @@ from tqdm import tqdm
 from dataloader import make_meta_files_dict
 
 
-def check_if_column_empty(column_name):
+def files_where_column_empty(column_name):
     """returns a list of all -meta.tsv files where the specified column contains something else than NaNs"""
-    files_where_column_empty = []
+    files_where_column_empty_list = []
     for path_list in tqdm(make_meta_files_dict().values()):
         for path in path_list:
             df = pd.read_csv(path, sep='\t')
             if not np.all([pd.isna(s) for s in df['Agenda']]):
-                files_where_column_empty.append(path)
-    return files_where_column_empty
+                files_where_column_empty_list.append(path)
+    return files_where_column_empty_list
 
 
 def values_in_column(column_name):
