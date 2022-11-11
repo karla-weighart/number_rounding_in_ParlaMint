0fafbacbd0f96bb0d4cc01ff017c511a9d3ff5cf
Karla Weighart <karla.weighart@gmail.com>
2022-11-11 13:59:45 +0100

    commented the code like the good programmer I am

--- understanding_the_corpus.py
+++ understanding_the_corpus.py
@@ -7,6 +7,7 @@ from dataloader import make_meta_files_dict
 
 
 def check_if_column_empty(column_name):
+    """returns a list of all -meta.tsv files where the specified column contains something else than NaNs"""
     files_where_column_empty = []
     for path_list in tqdm(make_meta_files_dict().values()):
         for path in path_list:
