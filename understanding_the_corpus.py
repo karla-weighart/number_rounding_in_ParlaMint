8fb8999700abc551caf16e8ea89b1e7b6c01d1e0
Karla Weighart <karla.weighart@gmail.com>
2022-11-11 14:37:12 +0100

    added very ugly function to find out which subcorpora exist

--- understanding_the_corpus.py
+++ understanding_the_corpus.py
@@ -15,3 +15,18 @@ def check_if_column_empty(column_name):
             if not np.all([pd.isna(s) for s in df['Agenda']]):
                 files_where_column_empty.append(path)
     return files_where_column_empty
+
+
+def check_subcorpora():
+    """checks if any meta file contains anything but 'Reference' or 'COVID' in the 'Subcorpus' column
+    spoiler alert: NOPE."""
+    files_where_other_subcorpus = []
+    for path_list in tqdm(make_meta_files_dict().values()):
+        for path in tqdm(path_list):
+            df = pd.read_csv(path, sep='\t')
+            mask1 = df['Subcorpus'] != 'Reference'
+            if not df[mask1]['ID'].shape == (0,):
+                mask2 = df[mask1]['Subcorpus'] != 'COVID'
+                if not df[mask1][mask2]['ID'].shape == (0,):
+                    files_where_other_subcorpus.append(path)
+    return files_where_other_subcorpus
