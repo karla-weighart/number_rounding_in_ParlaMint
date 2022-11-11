d4cba60926b626970c3db1d9ebbf8c1dba8fda48
Karla Weighart <karla.weighart@gmail.com>
2022-11-11 20:14:30 +0100

    renamed more stuff

--- understanding_the_corpus.py
+++ understanding_the_corpus.py
@@ -6,15 +6,15 @@ from tqdm import tqdm
 from dataloader import make_meta_files_dict
 
 
-def files_where_column_empty(column_name):
+def files_where_column_not_empty(column_name):
     """returns a list of all -meta.tsv files where the specified column contains something else than NaNs"""
-    files_where_column_empty_list = []
+    files_where_column_not_empty_list = []
     for path_list in tqdm(make_meta_files_dict().values()):
         for path in path_list:
             df = pd.read_csv(path, sep='\t')
             if not np.all([pd.isna(s) for s in df[column_name]]):
-                files_where_column_empty_list.append(path)
-    return files_where_column_empty_list
+                files_where_column_not_empty_list.append(path)
+    return files_where_column_not_empty_list
 
 
 def values_in_column(column_name):
