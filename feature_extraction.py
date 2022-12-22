a7f4f7fcaae06e7a65b0b7a2a178fb27d3d8a7eb
Karla Weighart <karla.weighart@gmail.com>
2022-12-22 13:40:02 +0100

    moved word counting to feature_extraction, deleted understanding_the_corpus

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,6 +6,26 @@ import conllu
 from typing import List, Tuple
 
 
+def count_words_in_sentence(sentence: conllu.models.TokenList) -> int:
+    """
+
+    Parameters
+    ----------
+    sentence: TokenList from the conllu parser
+
+    Returns
+    -------
+    number of words in that sentence
+    Does not count punctuation or genitive markers as words even though they have their own lines in the .conllu files.
+    """
+    sentence_df = pd.DataFrame(sentence)
+
+    filtered_df = sentence_df[(sentence_df['upos'] != 'PUNCT')
+                              & (sentence_df['lemma'] != '’s')
+                              & (sentence_df['lemma'] != '’')]
+    return filtered_df.shape[0]
+
+
 def find_pattern_in_sentence(pattern: List[Tuple[str]], sentence: conllu.models.TokenList) -> List[pd.DataFrame]:
     """
     Parameters
