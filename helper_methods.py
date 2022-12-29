7c45c4c2d5c88ebaaeb3f8d658a0c0844f1d4fc7
Karla Weighart <karla.weighart@gmail.com>
2022-12-29 15:34:12 +0100

    added is_enum

--- helper_methods.py
+++ helper_methods.py
@@ -15,3 +15,8 @@ def read_inner_dataframe(cell: str) -> pd.DataFrame:
     sentence DataFrame (= inner DataFrame)
     """
     return pd.DataFrame(literal_eval(cell))
+
+
+def is_enum(cell: pd.DataFrame) -> bool:
+    return cell.shape[0] == 2 and (cell['upos'].iloc[0], cell['upos'].iloc[1]) == ('X', 'PUNCT')
+
