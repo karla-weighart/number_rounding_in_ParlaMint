b60f0e09877b823487b60381eb11300c26c346f3
Karla Weighart <karla.weighart@gmail.com>
2022-12-30 13:00:30 +0100

    loading and generating should now yield the same format

--- load_saved_dataframe.py
+++ load_saved_dataframe.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-from helper_methods import read_inner_dataframe
+from ast import literal_eval
+
+from helper_methods import inner_dataframe_from_row
 
 
 def load_saved_df(path: str):
     saved_df = pd.read_csv(path, index_col=0)
-    saved_df['sentence'] = saved_df['sentence'].map(read_inner_dataframe)
+    saved_df['sentence'] = saved_df['sentence'].map(literal_eval)
     return saved_df
