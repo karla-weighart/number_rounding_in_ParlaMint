0ffbbb74389af412d6e650bae6dd6e52fa5aa293
Karla Weighart <karla.weighart@gmail.com>
2023-01-13 19:16:30 +0100

    used swifter for speedup

--- load_saved_dataframe.py
+++ load_saved_dataframe.py
@@ -5,5 +5,5 @@ from ast import literal_eval
 
 def load_saved_df(path: str):
     saved_df = pd.read_csv(path, index_col=0)
-    saved_df['sentence'] = saved_df['sentence'].map(literal_eval)
+    saved_df['sentence'] = saved_df['sentence'].swifter.apply(literal_eval)
     return saved_df
