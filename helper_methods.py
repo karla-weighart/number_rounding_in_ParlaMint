7f767700676d68dcd7df2d492ddc5dcfd61b477a
Karla Weighart <karla.weighart@gmail.com>
2023-01-13 23:47:04 +0100

    implemented support for 'needs manual inspection' cells in num_list. added documentation for contains_num and num_list

--- helper_methods.py
+++ helper_methods.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from typing import Union
+
 
 def is_enum(cell: dict) -> bool:
     sentence_df = pd.DataFrame(cell)
@@ -29,7 +31,36 @@ def count_words(cell: dict) -> int:
 
 
 def contains_num(cell: dict) -> bool:
+    """
+
+    Parameters
+    ----------
+    cell: sentence df as dict
+
+    Returns
+    -------
+    whether the sentence contains any 'NUM'
+    """
+
     # contains_digit = np.any([str(x) in '.'.join(cell['form']) for x in range(10)])
     # as later methods only work on 'NUM's, this yields too many false positives
     contains_numeral = 'NUM' in cell['upos']
     return contains_numeral  # or contains_digit
+
+
+def num_list(cell: dict) -> Union[list, str]:
+    """
+
+    Parameters
+    ----------
+    cell: sentence df as dict
+
+    Returns
+    -------
+    list of the forms of all 'NUM's in that sentence (which might already have been grouped + parsed before)
+    """
+
+    if type(cell) == str:
+        return "needs manual inspection"
+    sentence = pd.DataFrame(cell)
+    return list(sentence[sentence['upos'] == 'NUM']['form'])
