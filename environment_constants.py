32ea24ffae3d99f2751b71579c1b46167e126e1a
Karla Weighart <karla.weighart@gmail.com>
2023-01-19 10:37:00 +0100

    added all columns to load_saved_df for literal eval

--- environment_constants.py
+++ environment_constants.py
@@ -2,6 +2,27 @@ PATH = "C:\\Users\\karla\\Desktop\\Zula_Data"
 
 # columns that contain valuable information (I used understanding_the_corpus to identify those columns)
 # see dataset_info.md for more information about the content of these columns
-META_COLUMNS = ['ID', 'House', 'Speaker_role', 'Speaker_type', 'Speaker_party', 'Party_status',
-                'Speaker_name', 'Speaker_gender']
-SENTENCE_COLUMNS = ['form', 'upos', 'head']
+META_COLUMNS = ['ID',
+                'House',
+                'Speaker_role',
+                'Speaker_type',
+                'Speaker_party',
+                'Party_status',
+                'Speaker_name',
+                'Speaker_gender'
+                ]
+SENTENCE_COLUMNS = ['form',
+                    'upos',
+                    'head'
+                    ]
+
+# result columns that should be parsed if loading from csv
+LIT_EVAL_RESULT_COLUMNS = ['sentence_grouped_nums',
+                           'sentence_parsed_num_groups',
+                           'NUMs',
+                           'roundedness',
+                           'num_ancestors',
+                           'num_ancestor_set',
+                           'num_descendants',
+                           'num_descendant_set'
+                           ]
--- load_saved_dataframe.py
+++ load_saved_dataframe.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
 from ast import literal_eval
+from environment_constants import LIT_EVAL_RESULT_COLUMNS
 
 
 def load_saved_df(path: str):
     saved_df = pd.read_csv(path, index_col=0)
-    saved_df['sentence'] = saved_df['sentence'].swifter.apply(literal_eval)
+    for column in LIT_EVAL_RESULT_COLUMNS:
+        try:
+            saved_df[column] = saved_df[column].swifter.apply(literal_eval)
     return saved_df
