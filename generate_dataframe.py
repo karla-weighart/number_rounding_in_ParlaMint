9eb2fd2b9e86a7de6b2e6582fabc5e87bf017fb4
Karla Weighart <karla.weighart@gmail.com>
2023-02-27 17:44:15 +0100

    minor changes

--- generate_dataframe.py
+++ generate_dataframe.py
@@ -1,13 +1,11 @@
 import conllu
 import pandas as pd
-import swifter
 import random
 
 from glob import glob
 from tqdm.notebook import tqdm
 
 from helper_methods import is_enum, count_words, contains_num
-
 from environment_constants import PATH, META_COLUMNS, SENTENCE_COLUMNS
 
 
