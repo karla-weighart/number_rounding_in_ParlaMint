443dfbbf44f97fbb400a2f85865800edc0eddbb6
Karla Weighart <karla.weighart@gmail.com>
2023-03-16 17:12:07 +0100

    made it easier to set parameters for a run at a glance

--- tab2latex.py
+++ tab2latex.py
@@ -64,7 +64,7 @@ def tsv2subtable(table_content: str,
 
     return f"\\begin{{subtable}}[h]{{0.48\\textwidth}}\n\t\\centering\n\t" \
            f"{tabularx}\n\t\\" \
-           f"caption{{{caption}}}\n\t\\label{{}}\n\t\\label{{tab:{label}}}\n\t\\end{{subtable}}"
+           f"caption{{{caption}}}\n\t\\label{{tab:{label}}}\n\t\\end{{subtable}}"
 
 
 def comparison_table(text_incorrect: str,
@@ -91,18 +91,18 @@ def comparison_table(text_incorrect: str,
     # generate subtables and adjust indentation
     subtable_incorrect = tsv2subtable(text_incorrect,
                                       caption='Original Version',
-                                      label=f"tab:{label}-incorrect",
+                                      label=f"{label}-incorrect",
                                       colors={index: '\\clrincorrect' for index in corrected_indices}
                                       ).replace('\n', '\n\t')
 
     subtable_correct = tsv2subtable(text_correct,
                                     caption='Corrected Version',
-                                    label=f"tab:{label}-correct",
+                                    label=f"{label}-correct",
                                     colors={index: '\\clrcorrect' for index in corrected_indices}
                                     ).replace('\n', '\n\t')
 
     # put everything together
-    latex = f"\\begin{{table}}[]\n\t" \
+    latex = f"\\begin{{table}}[H]\n\t" \
             f"{subtable_incorrect}" \
             f"\n%\n\t\\hfill\n%\t\n" \
             f"{subtable_correct}" \
@@ -112,51 +112,72 @@ def comparison_table(text_incorrect: str,
 
 
 if __name__ == '__main__':
-    text_incorrect_ = """1	What	what	DET	WDT	PronType=Int	2	det	_	NER=O
-2	assessment	assessment	NOUN	NN	Number=Sing	5	obj	_	NER=O
-3	he	he	PRON	PRP	Case=Nom|Gender=Masc|Number=Sing|Person=3|PronType=Prs	5	nsubj	_	NER=O
-4	has	have	VERB	VBZ	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	5	aux	_	NER=O
-5	made	make	VERB	VBN	Tense=Past|VerbForm=Part	0	root	_	NER=O
-6	of	of	ADP	IN	_	14	mark	_	NER=O
-7	whether	whether	ADP	IN	_	14	mark	_	NER=O
-8	Zimbabwe	Zimbabwe	PROPN	NNP	Number=Sing	11	nmod:poss	_	NER=O|SpaceAfter=No
-9	’s	’s	PART	POS	_	8	case	_	NER=O
-10	next	next	ADJ	JJ	Degree=Pos	11	amod	_	NER=O
-11	election	election	NOUN	NN	Number=Sing	14	nsubj:pass	_	NER=O
-12	will	will	VERB	MD	VerbForm=Fin	14	aux	_	NER=O
-13	be	be	VERB	VB	VerbForm=Inf	14	aux:pass	_	NER=O
-14	conducted	conduct	VERB	VBN	Tense=Past|VerbForm=Part|Voice=Pass	5	advcl	_	NER=O
-15	freely	freely	ADV	RB	_	14	advmod	_	NER=O
-16	and	and	CCONJ	CC	_	17	cc	_	NER=O
-17	fairly	fairly	ADV	RB	_	14	conj	_	NER=O|SpaceAfter=No
-18	.	.	PUNCT	.	_	5	punct	_	NER=O
-"""
 
-    text_correct_ = """1	What	what	DET	WDT	PronType=Int	3	det	_	NER=O
-2	assessment	assessment	NOUN	NN	Number=Sing	5	obj	_	NER=O
-3	he	he	PRON	PRP	Case=Nom|Gender=Masc|Number=Sing|Person=3|PronType=Prs	5	nsubj	_	NER=O
-4	has	have	VERB	VBZ	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	5	aux	_	NER=O
-5	made	make	VERB	VBN	Tense=Past|VerbForm=Part	0	root	_	NER=O
-6	of	of	ADP	IN	_	14	mark	_	NER=O
-7	whether	whether	ADP	IN	_	14	mark	_	NER=O
-8	Zimbabwe	Zimbabwe	PROPN	NNP	Number=Sing	11	nmod:poss	_	NER=O|SpaceAfter=No
-9	’s	’s	PART	POS	_	8	case	_	NER=O
-10	next	next	ADJ	JJ	Degree=Pos	11	amod	_	NER=O
-11	election	election	NOUN	NN	Number=Sing	14	nsubj:pass	_	NER=O
-12	will	will	VERB	MD	VerbForm=Fin	14	aux	_	NER=O
-13	be	be	VERB	VB	VerbForm=Inf	14	aux:pass	_	NER=O
-14	conducted	conduct	VERB	VBN	Tense=Past|VerbForm=Part|Voice=Pass	5	advcl	_	NER=O
-15	freely	freely	ADV	RB	_	14	advmod	_	NER=O
-16	and	and	CCONJ	CC	_	17	cc	_	NER=O
-17	fairly	fairly	ADV	RB	_	14	conj	_	NER=O|SpaceAfter=No
-18	.	.	PUNCT	.	_	5	punct	_	NER=O
-"""
+    seg = 'ParlaMint-GB_2016-04-27-commons.seg22.1'
 
     corrected_indices_ = [1]
 
-    caption_ = 'testitestmann \\texttt{blablabla}'
+    text_incorrect_ = """1	Two	two	NUM	CD	NumType=Card	3	compound	_	NER=O
+2	hundred	hundred	NUM	CD	NumType=Card	3	compound	_	NER=O
+3	thousand	thousand	NUM	CD	NumType=Card	5	nummod	_	NER=O
+4	young	young	ADJ	JJ	Degree=Pos	5	amod	_	NER=O
+5	people	people	NOUN	NNS	Number=Plur	11	nsubj	_	NER=O
+6	have	have	VERB	VBP	Mood=Ind|Tense=Pres|VerbForm=Fin	11	aux	_	NER=O
+7	been	be	VERB	VBN	Tense=Past|VerbForm=Part	11	cop	_	NER=O
+8	through	through	ADP	IN	_	11	case	_	NER=O
+9	the	the	DET	DT	Definite=Def|PronType=Art	11	det	_	NER=O
+10	NCS	ncs	NOUN	NN	Number=Sing	11	compound	_	NER=B-ORG
+11	programme	programme	NOUN	NN	Number=Sing	0	root	_	NER=O
+12	so	so	ADV	RB	_	13	advmod	_	NER=O
+13	far	far	ADV	RB	Degree=Pos	11	advmod	_	NER=O
+14	and	and	CCONJ	CC	_	17	cc	_	NER=O
+15	we	we	PRON	PRP	Case=Nom|Number=Plur|Person=1|PronType=Prs	17	nsubj	_	NER=O
+16	are	be	VERB	VBP	Mood=Ind|Tense=Pres|VerbForm=Fin	17	aux	_	NER=O
+17	aiming	aim	VERB	VBG	Tense=Pres|VerbForm=Part	11	conj	_	NER=O
+18	to	to	PART	TO	_	19	mark	_	NER=O
+19	increase	increase	VERB	VB	VerbForm=Inf	17	xcomp	_	NER=O
+20	that	that	DET	DT	Number=Sing|PronType=Dem	21	det	_	NER=O
+21	number	number	NOUN	NN	Number=Sing	19	obj	_	NER=O
+22	significantly	significantly	ADV	RB	_	19	advmod	_	NER=O
+23	by	by	ADP	IN	_	24	case	_	NER=O
+24	2020	2020	NUM	CD	NumType=Card	19	obl	_	NER=O|SpaceAfter=No
+25	.	.	PUNCT	.	_	11	punct	_	NER=O
+"""
 
-    label_ = 'testlabel'
+    text_correct_ = """1	Two	two	NUM	CD	NumType=Card	2	compound	_	NER=O
+2	hundred	hundred	NUM	CD	NumType=Card	3	compound	_	NER=O
+3	thousand	thousand	NUM	CD	NumType=Card	5	nummod	_	NER=O
+4	young	young	ADJ	JJ	Degree=Pos	5	amod	_	NER=O
+5	people	people	NOUN	NNS	Number=Plur	11	nsubj	_	NER=O
+6	have	have	VERB	VBP	Mood=Ind|Tense=Pres|VerbForm=Fin	11	aux	_	NER=O
+7	been	be	VERB	VBN	Tense=Past|VerbForm=Part	11	cop	_	NER=O
+8	through	through	ADP	IN	_	11	case	_	NER=O
+9	the	the	DET	DT	Definite=Def|PronType=Art	11	det	_	NER=O
+10	NCS	ncs	NOUN	NN	Number=Sing	11	compound	_	NER=B-ORG
+11	programme	programme	NOUN	NN	Number=Sing	0	root	_	NER=O
+12	so	so	ADV	RB	_	13	advmod	_	NER=O
+13	far	far	ADV	RB	Degree=Pos	11	advmod	_	NER=O
+14	and	and	CCONJ	CC	_	17	cc	_	NER=O
+15	we	we	PRON	PRP	Case=Nom|Number=Plur|Person=1|PronType=Prs	17	nsubj	_	NER=O
+16	are	be	VERB	VBP	Mood=Ind|Tense=Pres|VerbForm=Fin	17	aux	_	NER=O
+17	aiming	aim	VERB	VBG	Tense=Pres|VerbForm=Part	11	conj	_	NER=O
+18	to	to	PART	TO	_	19	mark	_	NER=O
+19	increase	increase	VERB	VB	VerbForm=Inf	17	xcomp	_	NER=O
+20	that	that	DET	DT	Number=Sing|PronType=Dem	21	det	_	NER=O
+21	number	number	NOUN	NN	Number=Sing	19	obj	_	NER=O
+22	significantly	significantly	ADV	RB	_	19	advmod	_	NER=O
+23	by	by	ADP	IN	_	24	case	_	NER=O
+24	2020	2020	NUM	CD	NumType=Card	19	obl	_	NER=O|SpaceAfter=No
+25	.	.	PUNCT	.	_	11	punct	_	NER=O
+"""
+
+
+
+###################
+
+    caption_ = f'Original and Corrected Version of Sentence \\texttt{{{seg}}}'.replace('_', '\_')
+
+    label_ = f"appendixWrongDependencies{seg[len('ParlaMint-GB_'):]}"
 
     print(comparison_table(text_incorrect=text_incorrect_,
                            text_correct=text_correct_,
